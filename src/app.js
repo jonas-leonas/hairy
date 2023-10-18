7c2365a13a62acaaee45566628f6fea439cc52eb
Jonas Leonas <leopoldaz@gmail.com>
2023-10-18 11:02:36 +0200

    "api v1.2 test local data in JSON"

--- src/app.js
+++ src/app.js
@@ -1,3 +1,34 @@
-const {v4: uuidv4} = require('uuid');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-console.log(uuidv4());
+const customers = [
+    {
+        "name": "Caleb",
+        "industry": "music"
+    },
+    {
+        "name": "John",
+        "industry": "networking"
+    },
+    {
+        "name": "Sal",
+        "industry": "sports medicine"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
+
+});
+
+app.post('/', (req, res) =>{
+    res.send('This is a post request!');
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on port: ' + PORT); 
+});
